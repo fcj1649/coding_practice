342c01de7919652fe3607807095b0b00922c8277
fcj1649 <eswaramaharaja@gmail.com>
2023-12-06 17:01:19 -0500

    spoj - palin

--- SPOJ/spoj_class_5_palin.py
+++ SPOJ/spoj_class_5_palin.py
@@ -1,9 +1,15 @@
 import math
-test_case = int(input())
-for i in range(0, test_case):
-    data = str(input())
-    out = ""
-    num = [int(i) for i in data]
+
+def copy_num(num : list, left : int, right: int):
+    while( left >= 0 and right < len(num)):
+        num[right] = num[left]
+        left = left - 1
+        right = right + 1
+    return num
+
+def next_palin(input: str):
+    num = [int(i) for i in input]
+    
     if(len(num) == 1):
         # only 1 digit the next palindrome is the next number if < 9 else if its 9 then 11
         if(num[0] == 9):
@@ -19,19 +25,34 @@ for i in range(0, test_case):
         mid = math.floor(len(num) / 2)
         left = mid - 1
         right = mid + 1
+        suceess = False
         while( left >= 0 and right < len(num)):
             if(num[left] > num[right]):
                 #left number is greater no need to change mid
                 #copy all numbers from left to right
-                left = left
+                num = copy_num(num, left, right)
+                suceess = True
+                break
             elif(num[left] < num[right]):
                 #left number is smaller than right
                 #add one to mid
                 num[mid] = num[mid] + 1
+                num = copy_num(num, left, right)
+                suceess = True
+                break
             else:    
                 left = left - 1
                 right = right + 1
-             
 
-    out = out.join((str(x) for x in num))
-    print(out)
+        if(not suceess):
+            # number is already a palindrome
+            num[mid] = num[mid] + 1
+    return "".join((str(x) for x in num))
+
+def main():
+    test_case = int(input())
+    for i in range(0, test_case):
+        print(next_palin(str(input())))
+
+if __name__ == "__main__":
+    main()
--- SPOJ/spoj_class_5_palin_test.py
+++ SPOJ/spoj_class_5_palin_test.py
@@ -1,17 +1,25 @@
 import math
+import spoj_class_5_palin
 
 def rev(num):
 	return int(num != 0) and ((num % 10) * \
 			(10**int(math.log(num, 10))) + \
 						rev(num // 10))
 
-test_number = 9288811
+try:
+	test_case = int(input())
+except EOFError:
+	test_case = 0
 
-new_number = test_number
-res = False 
+for i in range(test_case):
+	test_number = int(input())
+	new_number = test_number
+	res = False 
 
-while(not res):
-    new_number = new_number + 1
-    res = new_number == rev(new_number)
+	while(not res):
+		new_number = new_number + 1
+		res = new_number == rev(new_number)
 
-print (str( test_number ) + " Next palindrome:" + str(new_number))
+	act_res = spoj_class_5_palin.next_palin(str(test_number))
+	final_res = act_res == str(new_number) 
+	print (str(final_res) + " Inp:" + str(test_number) + " Exp:" + str(new_number) + " Act:" + act_res)
