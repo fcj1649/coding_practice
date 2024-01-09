fa7a102824bee5fb6d17c996278b18604201c52b
fcj1649 <eswaramaharaja@gmail.com>
2024-01-09 14:48:21 -0500

    euler - 2 - optiz

--- ProjectEuler/Solved/proj_euler_2.py
+++ ProjectEuler/Solved/proj_euler_2.py
@@ -1,13 +1,13 @@
 f1 = 1 
 f2 = 2
 f3 = 3
-sum = 2
-while( f3 <= 4000000):
+fib_sum = 2
+while( f3 <= 4 * 10 ** 6):
     f1 = f2
     f2 = f3
     f3 = f1 + f2
 
     if( f3 % 2 == 0):
-        sum = sum + f3
+        fib_sum = fib_sum + f3
 
-print(sum)
+print(fib_sum)
