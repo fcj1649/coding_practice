ec9232da643d8389b4678d562f547b7ca7404840
fcj1649 <eswaramaharaja@gmail.com>
2024-01-10 16:08:14 -0500

    euler - 5 opti

--- ProjectEuler/Solved/proj_euler_5.py
+++ ProjectEuler/Solved/proj_euler_5.py
@@ -1,46 +1,15 @@
-max = 20
+n = 20
+inp = [0] * (n + 1)
 
-def checkOne(num: list):
-    valid = True
-    for i in range(1, len(num)):
-        if(num[i]!=1):
-            valid = False
-            break
-    return valid
+for i in range(1, len(inp)):
+    inp[i] = i
 
-list_num = [True] * max
+for i in range(2, n+1):
+    for j in range(i+1, n+1):
+        if(inp[j] % inp[i] == 0):
+            inp[j] = inp[j] // inp[i]
 
-list_num[0] = False
-list_num[1] = False
-
-for i in range(2, max):
-    if(list_num[i]):
-        j = i * 2
-        while( j < max):
-            list_num[j] = False
-            j = j + i
-
-list = [0] * (max + 1)
-
-for i in range(len(list)):
-    list[i] = i
-
-out = []
-
-while(not checkOne(list)):
-    for idx, num in enumerate(list_num):
-        if(num): 
-            #prime
-            isDiv = False
-            for i in range(1 , len(list)):
-                if(list[i] % idx == 0):
-                    list[i] = int(list[i] / idx)
-                    isDiv = True
-            if(isDiv):
-                out.append(idx)
 prod = 1
-for x in out:
-    prod *= x
-print(out)
-print(prod)
-
+for i in range(1, len(inp)):
+    prod *= inp[i]
+print(inp, prod)
