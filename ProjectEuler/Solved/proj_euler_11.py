c2fa4ec0b6dcc311cab35e5565f74990115407d9
fcj1649 <eswaramaharaja@gmail.com>
2024-01-11 17:20:54 -0500

    euler optiz

--- ProjectEuler/Solved/proj_euler_11.py
+++ ProjectEuler/Solved/proj_euler_11.py
@@ -1,12 +1,9 @@
 file = open('.\ProjectEuler\Solved\proj_euler_11.txt', 'r')
-grid = [[]]
+grid = []
 for line in file.readlines():
     nums = [int(x) for x in line.strip().split(" ")]
     grid.append(nums)
-grid.pop(0)
 print(grid)
-
-prod = -1
 
 for i in range(0, len(grid)):
     for j in range(3, len(grid[i])):
@@ -18,10 +15,10 @@ for i in range(0, len(grid)):
 
 for i in range(3, len(grid)):
     for j in range(3, len(grid[i])):
-        prod = max(prod, grid[i][j] * grid[i-1][j-1] * grid[i-2][j-2] * grid[i-3][j-3])                
+        prod = max(prod, grid[i][j] * grid[i-1][j-1] * grid[i-2][j-2] * grid[i-3][j-3])
 
 for i in range(0, len(grid) - 3):
     for j in range(3, len(grid[i])):
-        prod = max(prod, grid[i][j] * grid[i+1][j-1] * grid[i+2][j-2] * grid[i+3][j-3])   
+        prod = max(prod, grid[i][j] * grid[i+1][j-1] * grid[i+2][j-2] * grid[i+3][j-3])
 
 print(prod)
--- ProjectEuler/Solved/proj_euler_15.py
+++ ProjectEuler/Solved/proj_euler_15.py
@@ -1,17 +1,18 @@
 max = 20
-len = max+1
+length = max+1
 wid = max+1
-grid = [[0]*len for _ in range(wid)]
+grid = [[0]*length for _ in range(wid)]
 
-for i in range(len):
+for i in range(length):
     grid[i][0] = 1
     grid[0][i] = 1
 
 grid[0][0] = 0
 
-for i in range(1, len):
+for i in range(1, length):
     for j in range(1, wid):
         grid[i][j] = grid[i-1][j] + grid[i][j-1]
 
-print(grid)
-print(grid[len-1][wid-1])
+for x in grid:
+    print(x)
+print(grid[length-1][wid-1])
