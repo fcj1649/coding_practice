315b09330418c68c344cbebc41c35680529264f0
fcj1649 <eswaramaharaja@gmail.com>
2024-01-11 14:07:27 -0500

    euler - opti - 14

--- ProjectEuler/Solved/proj_euler_14.py
+++ ProjectEuler/Solved/proj_euler_14.py
@@ -1,35 +1,4 @@
 import time
-
-# start_time = time.time()
-
-# seq = {1 : 1}
-# max = 10 ** 6
-# max_seq = 1
-# out = 1
-# for i in range (2, max):
-#     # check if seq already exists
-#     count = 0
-#     num = i
-#     while(num != 1):
-#         if(num in seq):
-#             count = count + seq[num] - 1
-#             break
-#         else:
-#             count = count + 1
-
-#         if (num % 2 == 0):
-#             num = num // 2
-#         else:
-#             num = 3 * num + 1
-    
-#     seq[i] = count + 1
-#     if(count > max_seq):
-#         max_seq = count
-#         out = i
-# print(out)
-# end_time = time.time()
-# print("--- %s seconds ---" % (end_time - start_time))
-
 def collatz_seq(seq_list: list, inp_num: int, max_list: list):
     out = max_list[len(max_list)-1]
     max_ret = seq_list[out]
