d2537c4ad872e90c0ad89ab70e5a5140809e35c1
fcj1649 <eswaramaharaja@gmail.com>
2026-05-07 14:04:14 -0400

    normal sync

--- SPOJ/Solved/spoj_class_1112_nsteps.py
+++ SPOJ/Solved/spoj_class_1112_nsteps.py
@@ -22,3 +22,21 @@ for i in range(0, test_case):
             print(x + y - 1)
     else:
         print("No Number")
+
+"""
+XC	YC	X+y	x-y	X-E/O	No	%4
+0	0	0	0	EVEN	0	0
+1	1	2	0	ODD	    1	1
+2	0	2	2	EVEN	2	2
+3	1	4	2	ODD	    3	3
+2	2	4	0	EVEN	4	0
+3	3	6	0	ODD	    5	1
+4	2	6	2	EVEN	6	2
+5	3	8	2	ODD	    7	3
+4	4	8	0	EVEN	8	0
+5	5	10	0	ODD	    9	1
+6	4	10	2	EVEN	10	2
+7	5	12	2	ODD	    11	3
+6	6	12	0	EVEN	12	0
+7	7	14	0	ODD	    13	1
+"""
